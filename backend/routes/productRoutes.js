a74619852c149005c66b021b58ccafaf5b544846
TraxGGDev <traxgg17@gmail.com>
2025-05-20 14:33:09 -0600

    add get products function

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { createProduct } from '../controllers/productController.js';
+import { createProduct, getAllProducts } from '../controllers/productController.js';
+
 
 const router = express.Router();
 
+//crear producto nuevo
 router.post('/create', createProduct );
-
+//obtener todos los productos
+router.get('/', getAllProducts);
 export default router;
